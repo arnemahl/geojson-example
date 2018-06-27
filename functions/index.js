b5a6dd5ef6a7c6f5c2860eae9f55dc54e898cd26
Arne Mæhlum <arne.mahlum@gmail.com>
2018-06-27 14:26:17 +0200

    Create endpoint addOperation

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const union = require('@turf/union');
+const intersect = require('@turf/intersect');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,81 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+function applyOperation(geojson, operation) {
+  const selection = geojson.features.filter((_, index) => operation.selection.includes(index));
+  const combineFn = { union, intersect }[operation.name];
+
+  const arrNotNull = (val) => val === null ? [] : val;
+
+  return {
+    ...geojson,
+    features: geojson.features
+      .filter(remaining => !selection.includes(remaining))
+      .concat(arrNotNull(combineFn(...selection))),
+  };
+}
+
+function isValid(geojson) {
+  return true; // I don't know how to validate GeoJSON
+}
+
+// Based on example:
+// https://firebase.google.com/docs/functions/get-started#add_the_addmessage_functions
+exports.addOperation = functions.https.onRequest((req, res) => {
+  // We only have this one project for now, but that could change.
+  const projectName = 'sample-project';
+
+  const projectRef =
+    admin
+      .database()
+      .ref('projects')
+      .child(projectName)
+  ;
+
+  // If we add more data to projects we might want to only fetch initialGeoJson and operations
+  // here, and not anything extra. That must be done in two operations, though.
+  projectRef.once('value', snap => {
+    const project = snap.val();
+
+    if (!project) {
+      res.status(404).json({ error: `Not Found: The project "${projectName}" does not exist.` });
+      return;
+    }
+
+    const { initialGeoJson, operations = [] } = project;
+
+    const currentGeoJson = operations.reduce(applyOperation, initialGeoJson);
+
+    const nextOperation = {
+      name: req.query.name,
+      selection: req.query.selection,
+    };
+
+    if (nextOperation.selection.some(index => currentGeoJson.features[index] !== 'object')) {
+      res.status(400).json({ error: `Bad Request: The selection contains nonexistent features.` });
+      return;
+    }
+    if (['union', 'intersect'].includes(nextOperation.name)) {
+      res.status(400).json({ error: `Bad Request: The operation "${nextOperation.name}" is not supported.` });
+      return;
+    }
+
+    const nextGeoJson = applyOperation(currentGeoJson, nextOperation);
+
+    if (!isValid(nextGeoJson)) {
+      res.status(400).json({ error: `Bad Request: Applying the operation would produce an invalid GeoJSON.` });
+      return;
+    }
+
+    projectRef
+      .child('operations')
+      .push(nextOperation)
+      .then(() => {
+        res.status(200).json({ message: 'OK' });
+      })
+      .catch(error => {
+        res.status(500).json({ error });
+      });
+  });
+});
