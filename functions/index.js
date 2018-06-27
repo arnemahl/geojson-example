843da3d37bc411fea843cefafde5f42a12291a42
Arne Mæhlum <arne.mahlum@gmail.com>
2018-06-27 15:16:11 +0200

    Comment out stuff

--- functions/index.js
+++ functions/index.js
@@ -57,33 +57,35 @@ exports.addOperation = functions.https.onRequest((req, res) => {
 
       const { initialGeoJson, operations = [] } = project;
 
-      const currentGeoJson = operations.reduce(applyOperation, initialGeoJson);
+      // const currentGeoJson = operations.reduce(applyOperation, initialGeoJson);
 
-      const nextOperation = JSON.parse(decodeURIComponent(req.query));
+      // const nextOperation = JSON.parse(decodeURIComponent(req.query));
 
-      if (nextOperation.selection.length !== 2) {
-        res.status(400).json({ error: `Bad Request: Currently only supports operations on exactly two features.` });
-        return;
-      }
-      if (nextOperation.selection.some(index => currentGeoJson.features[index] !== 'object')) {
-        res.status(400).json({ error: `Bad Request: The selection contains nonexistent features.` });
-        return;
-      }
-      if (['union', 'intersect'].includes(nextOperation.name)) {
-        res.status(400).json({ error: `Bad Request: The operation "${nextOperation.name}" is not supported.` });
-        return;
-      }
+      // if (nextOperation.selection.length !== 2) {
+      //   res.status(400).json({ error: `Bad Request: Currently only supports operations on exactly two features.` });
+      //   return;
+      // }
+      // if (nextOperation.selection.some(index => currentGeoJson.features[index] !== 'object')) {
+      //   res.status(400).json({ error: `Bad Request: The selection contains nonexistent features.` });
+      //   return;
+      // }
+      // if (['union', 'intersect'].includes(nextOperation.name)) {
+      //   res.status(400).json({ error: `Bad Request: The operation "${nextOperation.name}" is not supported.` });
+      //   return;
+      // }
 
-      const nextGeoJson = applyOperation(currentGeoJson, nextOperation);
+      // const nextGeoJson = applyOperation(currentGeoJson, nextOperation);
 
-      if (!isValid(nextGeoJson)) {
-        res.status(400).json({ error: `Bad Request: Applying the operation would produce an invalid GeoJSON.` });
-        return;
-      }
+      // if (!isValid(nextGeoJson)) {
+      //   res.status(400).json({ error: `Bad Request: Applying the operation would produce an invalid GeoJSON.` });
+      //   return;
+      // }
 
       projectRef
         .child('operations')
-        .push(nextOperation)
+        .set(
+          operations.concat([{ name: 'union', selection: [0, 1] }])
+        )
         .then(() => { // eslint-disable-line promise/always-return
           res.status(200).json({ message: 'OK' });
         })
